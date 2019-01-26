8057bb4335a73669c0cffd62d447d0e361e749ee
YBZMZM <2251729740@qq.com>
2019-01-26 14:56:31 +0800

    update

--- String/面试题38-2：字符串的组合/StringsCombination.cpp
+++ String/面试题38-2：字符串的组合/StringsCombination.cpp
@@ -1,77 +1,32 @@
-// 面试题38：字符串的排列
-// 题目：输入一个字符串，打印出该字符串中字符的所有排列。例如输入字符串abc，
-// 则打印出由字符a、b、c所能排列出来的所有字符串abc、acb、bac、bca、cab和cba。
-
-#include <stdio.h>
-
-void Permutation(char* pStr, char* pBegin);
-
-void Permutation(char* pStr)
+#include<stdio.h>
+#include<string.h>
+void Show(char *str,int begin,int end)
 {
-	if (pStr == NULL)
+	char *pStr = str;
+	for (int i = begin; i < end; i++)
 	{
-		return;
+		printf("%c",pStr[i]);
 	}
-	Permutation(pStr,pStr);
+	printf("\n");
 }
 
-void Permutation(char* pStr, char* pBegin)
+void Combine(char *str,int length)
 {
-	if (*pBegin == '\0')
+	char *pStr = str;
+	for (int i = 0; i < length; i++)
 	{
-		printf("%s\n",pStr);
-	}
-	else
-	{
-		for (char *pCh = pBegin; *pCh != '\0'; pCh++)
+		for (int j = i; j < length; j++)
 		{
-			char temp = *pCh;
-			*pCh = *pBegin;
-			*pBegin = temp;
-
-			Permutation(pStr,pBegin+1);
-
-			temp = *pCh;
-			*pCh = *pBegin;
-			*pBegin = temp;
+			int begin = i;
+			int end = j;
+			Show(pStr,begin,end);
 		}
 	}
 }
 
-// ====================测试代码====================
-void Test(char* pStr)
+int main()
 {
-    if(pStr == nullptr)
-        printf("Test for nullptr begins:\n");
-    else
-        printf("Test for %s begins:\n", pStr);
-
-    Permutation(pStr);
-
-    printf("\n");
+	char str[] = "abcd";
+	int length = sizeof(str)/sizeof(str[0]);
+	Combine(str,length);
 }
-
-int main(int argc, char* argv[])
-{
-    Test(nullptr);
-
-    char string1[] = "";
-    Test(string1);
-
-    char string2[] = "a";
-    Test(string2);
-
-    char string3[] = "ab";
-    Test(string3);
-
-    char string4[] = "abc";
-    Test(string4);
-
-	char string5[] = "abcd";
-    Test(string5);
-
-	char string6[] = "abcde";
-    Test(string6);
-    return 0;
-}
-
